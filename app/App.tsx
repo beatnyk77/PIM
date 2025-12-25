f5fd6ed54aa5c93f26fc6ae59964d71c91f68f1c
beatnyk77 <beatnyk77@gmail.com>
2025-12-25 22:14:48 +0530

    Task 5: Create App Root Component

--- app/App.tsx
+++ app/App.tsx
@@ -1,12 +1,13 @@
 import "./global.css";
 import { StatusBar } from 'expo-status-bar';
+import { SafeAreaProvider } from 'react-native-safe-area-context';
 import AppNavigator from './navigation';
 
 export default function App() {
   return (
-    <>
+    <SafeAreaProvider>
       <AppNavigator />
       <StatusBar style="auto" />
-    </>
+    </SafeAreaProvider>
   );
 }
